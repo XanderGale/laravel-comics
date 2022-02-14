d61f27bdf48eaf11037c0287547a68ad865c21a7
XanderGale <alessandro.galeazzi@icloud.com>
2022-02-14 16:50:52 +0100

    class by route name

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,31 +5,31 @@
             <img src="{{ asset('images/dc-logo.png') }}" alt="DC Logo">
 
             <ul>
-                <li>
+                <li class="{{ Request::route()->getName() === 'characters' ? 'active' : '' }}">
                     <a href="#">Characters</a>
                 </li>
-                <li class="active">
+                <li class="{{ Request::route()->getName() === 'homepage' ? 'active' : '' }}">
                     <a href="#">Comics</a>
                 </li>
-                <li>
+                <li class="{{ Request::route()->getName() === 'movies' ? 'active' : '' }}">
                     <a href="#">Movies</a>
                 </li>
-                <li>
+                <li class="{{ Request::route()->getName() === 'tv' ? 'active' : '' }}">
                     <a href="#">Tv</a>
                 </li>
-                <li>
+                <li class="{{ Request::route()->getName() === 'games' ? 'active' : '' }}">
                     <a href="#">Games</a>
                 </li>
-                <li>
+                <li class="{{ Request::route()->getName() === 'videos' ? 'active' : '' }}">
                     <a href="#">Videos</a>
                 </li>
-                <li>
+                <li class="{{ Request::route()->getName() === 'fans' ? 'active' : '' }}">
                     <a href="#">Fans</a>
                 </li>
-                <li>
+                <li class="{{ Request::route()->getName() === 'news' ? 'active' : '' }}">
                     <a href="#">News</a>
                 </li>
-                <li>
+                <li class="{{ Request::route()->getName() === 'shop' ? 'active' : '' }}">
                     <a href="#">Shop</a>
                 </li>
             </ul>
